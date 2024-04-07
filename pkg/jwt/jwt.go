f8e5299beeab2e42b1dad2e585b7efb97d851f69
shaozheng.chang <shaozheng.chang@nio.com>
2024-04-07 14:51:33 +0800

    update jwt

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,6 +1,7 @@
 package jwt
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -37,15 +38,20 @@ func NewJwtTokenUtil(secret string, expiration int64, tokenHead string) *JwtToke
 	}
 }
 
+// generateToken 根据负责生成JWT的token
+func (j *JwtTokenUtil) generateToken(claims jwt.MapClaims) (string, error) {
+	claims[CLAIM_KEY_EXP] = time.Now().Add(time.Duration(j.Expiration) * time.Second).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+	return token.SignedString([]byte(j.Secret))
+}
+
 // GenerateToken 根据用户信息生成JWT的token
 func (j *JwtTokenUtil) GenerateToken(username string) (string, error) {
 	claims := jwt.MapClaims{
 		CLAIM_KEY_USERNAME: username,
 		CLAIM_KEY_CREATED:  time.Now().Unix(),
-		CLAIM_KEY_EXP:      time.Now().Add(time.Duration(j.Expiration) * time.Second).Unix(),
 	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
-	return token.SignedString([]byte(j.Secret))
+	return j.generateToken(claims)
 }
 
 // GetClaimsFromToken 从token中获取JWT中的负载
@@ -61,19 +67,102 @@ func (j *JwtTokenUtil) GetClaimsFromToken(tokenString string) (jwt.MapClaims, er
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		return claims, nil
 	} else {
-		return nil, jwt.NewValidationError("Token is invalid", jwt.ValidationErrorSignatureInvalid)
+		return nil, jwt.ErrTokenSignatureInvalid
 	}
 }
 
 // ValidateToken 验证token是否还有效
-func (j *JwtTokenUtil) ValidateToken(tokenString string, username string) (bool, error) {
+func (j *JwtTokenUtil) ValidateToken(tokenString string, username string) bool {
+	user := j.GetUserNameFromToken(tokenString)
+	return user == username && !j.isTokenExpired(tokenString)
+}
+
+// isTokenExpired 判断token是否已经失效
+func (j *JwtTokenUtil) isTokenExpired(tokenString string) bool {
+	expiration := j.getExpiredDateFromToken(tokenString)
+	return time.Now().After(expiration)
+}
+
+// getExpiredDateFromToken 从token中获取过期时间
+func (j *JwtTokenUtil) getExpiredDateFromToken(tokenString string) time.Time {
 	claims, err := j.GetClaimsFromToken(tokenString)
 	if err != nil {
-		return false, err
+		return time.Time{}
+	}
+	expUnix, ok := claims[CLAIM_KEY_EXP].(float64)
+	if !ok {
+		return time.Time{}
+	}
+	expiration := time.Unix(int64(expUnix), 0)
+	return expiration
+}
+
+// GetUserNameFromToken 从token中获取登录用户名
+func (j *JwtTokenUtil) GetUserNameFromToken(tokenString string) string {
+	var username string
+	claims, err := j.GetClaimsFromToken(tokenString)
+	if err != nil {
+		return ""
 	}
 
-	user := claims[CLAIM_KEY_USERNAME].(string)
-	expiration := time.Unix(int64(claims[CLAIM_KEY_EXP].(float64)), 0)
+	username, ok := claims[CLAIM_KEY_USERNAME].(string)
+	if !ok {
+		return ""
+	}
+	return username
+}
 
-	return user == username && time.Now().Before(expiration), nil
+// RefreshHeadToken 根据旧令牌刷新新令牌
+func (j *JwtTokenUtil) RefreshHeadToken(oldToken string, interval int) (string, error) {
+	if oldToken == "" {
+		return oldToken, errors.New("token is empty")
+	}
+
+	// 解析旧令牌
+	claims, err := j.GetClaimsFromToken(oldToken)
+	if err != nil {
+		return oldToken, err
+	}
+
+	// 检查令牌是否过期
+	if j.isTokenExpired(oldToken) {
+		return oldToken, jwt.ErrTokenExpired
+	}
+
+	// 检查令牌是否在30分钟内刚刷新过
+	if j.tokenRefreshJustBefore(oldToken, interval) {
+		return oldToken, nil // 返回原令牌
+	}
+
+	claims[CLAIM_KEY_CREATED] = time.Now().Unix()
+	// 生成新令牌
+	newTokenString, err := j.generateToken(claims)
+	if err != nil {
+		return "", err
+	}
+	return newTokenString, nil
+}
+
+/**
+ * 判断token在指定时间内是否刚刚刷新过
+ * @param token 原token
+ * @param offsetSecond 指定时间（秒）
+ */
+func (j *JwtTokenUtil) tokenRefreshJustBefore(token string, offsetSecond int) bool {
+	claims, err := j.GetClaimsFromToken(token)
+	if err != nil {
+		return false
+	}
+
+	createdUnix, ok := claims[CLAIM_KEY_CREATED].(float64)
+	if !ok {
+		return false
+	}
+	created := time.Unix(int64(createdUnix), 0)
+	refreshDate := time.Now()
+	// 刷新时间在创建时间的指定时间内
+	if refreshDate.After(created) && refreshDate.Before(created.Add(time.Duration(offsetSecond)*time.Second)) {
+		return true
+	}
+	return false
 }
---
+++ pkg/jwt/jwt_test.go
@@ -0,0 +1,53 @@
+package jwt
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJwtTokenUtil(t *testing.T) {
+	tests := []struct {
+		name     string
+		username string
+	}{
+		{
+			name:     "test1",
+			username: "jack",
+		},
+	}
+	var jwtUtil = NewJwtTokenUtil("123456", 2, "Bearer ")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			token, err := jwtUtil.GenerateToken(tt.username)
+			assert.Equal(t, nil, err)
+
+			username := jwtUtil.GetUserNameFromToken(token)
+			assert.Equal(t, tt.username, username, "应该解析出用户名")
+
+			expiration := jwtUtil.getExpiredDateFromToken(token)
+			assert.NotEqual(t, time.Time{}, expiration, "应该解析出过期时间")
+
+			ok := jwtUtil.ValidateToken(token, "nick")
+			assert.Equal(t, false, ok)
+
+			ok = jwtUtil.ValidateToken(token, "jack")
+			assert.Equal(t, true, ok)
+
+			newToken, err := jwtUtil.RefreshHeadToken(token, 1)
+			assert.Equal(t, nil, err)
+			assert.Equal(t, token, newToken, "无需刷新, 返回的应该是原始token")
+
+			time.Sleep(time.Second * 1)
+			newToken, err = jwtUtil.RefreshHeadToken(token, 1)
+			assert.Equal(t, nil, err)
+			assert.NotEqual(t, token, newToken, "token应该刷新")
+
+			// token过期失效
+			time.Sleep(time.Second * 2)
+			ok = jwtUtil.ValidateToken(token, "jack")
+			assert.Equal(t, false, ok, "token应该过期")
+		})
+	}
+}
